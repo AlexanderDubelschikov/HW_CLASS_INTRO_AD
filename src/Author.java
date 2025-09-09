bdd16bcc1e0c507c4c718147c6a07f07627e44c7
AlexanderDubelschikov <109314029+AlexanderDubelschikov@users.noreply.github.com>
2025-09-09 16:24:48 +0300

    HW-6-Reformat

--- src/Author.java
+++ src/Author.java
@@ -4,7 +4,7 @@ public class Author {
     public String surname;
     public int rating;
 
-    public Author(String name, String surname, int rating){
+    public Author(String name, String surname, int rating) {
         this.name = name;
         this.surname = surname;
         this.rating = rating;
